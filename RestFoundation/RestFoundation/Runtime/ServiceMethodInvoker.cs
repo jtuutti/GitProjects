4916885d0bbf31d30c93e8f119d0ac91e4b929b2
Dmitry <Dmitry>
2012-07-12 00:03:05 -0400

    Added response header behavior

--- RestFoundation/RestFoundation/Runtime/ServiceMethodInvoker.cs
+++ RestFoundation/RestFoundation/Runtime/ServiceMethodInvoker.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Web;
 
 namespace RestFoundation.Runtime
 {
--- RestFoundation/RestTest/App_Start/ServiceBootstrapper.cs
+++ RestFoundation/RestTest/App_Start/ServiceBootstrapper.cs
@@ -22,6 +22,7 @@ namespace RestTest.App_Start
             Rest.Configure.WithObjectFactory(CreateObjectFactory, CreateObjectBuilder)
                           .WithDataFormatters(RegisterDataFormatters)
                           .WithRoutes(RegisterRoutes)
+                          .WithGlobalBehaviors(b => b.AddGlobalBehaviors(new ResponseHeaderBehavior("REST Foundation Service", "Test Service")))
                           .EnableJsonPSupport()
                           .EnableServiceProxyUI();
         }
