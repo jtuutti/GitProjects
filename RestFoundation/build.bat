e147b876a86f9772b336378c6348e6428576c64d
Dmitry <Dmitry>
2012-08-03 01:31:29 -0400

    Changed paths

--- RestFoundation/build.bat
+++ RestFoundation/build.bat
@@ -1,16 +1,18 @@
 @echo off
 del .\RestFoundation\bin\Release\*.* /f /q
 del .\RestFoundation\obj\Release\*.* /f /q
-del .\Dependencies\Rest.Foundation.* /f /q
-pause
+mkdir .\build > nul
+del .\build\Rest.Foundation.* /f /q > nul
+
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild RestFoundation\RestFoundation.csproj /t:Rebuild /p:Configuration=Release
 cd .\RestFoundation\bin\Release
+
 echo.
 echo.
 echo Performing IL Merge
 ilmerge /out:Rest.Foundation.dll RestFoundation.dll Newtonsoft.Json.dll /ver:1.0.0.0 /v4 /keyfile:..\..\..\RestFoundation.snk /xmldocs
 echo.
 echo.
-copy Rest.Foundation.* ..\..\..\Dependencies\ /y
+copy Rest.Foundation.* ..\..\..\build\ /y
 echo.
 pause
