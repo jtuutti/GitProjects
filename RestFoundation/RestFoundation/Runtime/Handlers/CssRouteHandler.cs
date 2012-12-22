61d5bb8cfd65046f3ef52c2ae2bba9be1154aebf
Dmitry <Dmitry>
2012-12-22 15:41:23 -0500

    Added CSS/JS resources

---
+++ RestFoundation/RestFoundation/Runtime/Handlers/CssRouteHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Web;
+using System.Web.Routing;
+
+namespace RestFoundation.Runtime.Handlers
+{
+    internal class CssRouteHandler : IRouteHandler, IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return this;
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            using (var jqueryStream = typeof(Rest).Assembly.GetManifestResourceStream("RestFoundation.ServiceProxy.Resources.help.css"))
+            {
+                if (jqueryStream == null)
+                {
+                    throw new HttpException(404, "CSS resource not found");
+                }
+
+                context.Response.ContentType = "text/css";
+                context.Response.Cache.SetCacheability(HttpCacheability.Public);
+                context.Response.Cache.SetETag(GetAssemblyVersionAsEtag());
+                context.Response.Cache.SetExpires(DateTime.Now.AddYears(1));
+                context.Response.Cache.SetValidUntilExpires(true);
+
+                jqueryStream.CopyTo(context.Response.OutputStream);
+            }
+        }
+
+        private string GetAssemblyVersionAsEtag()
+        {
+            Version assemblyVersion = GetType().Assembly.GetName().Version;
+
+            return String.Format(CultureInfo.InvariantCulture,
+                                 "\"CSS-{0}-{1}-{2}-{3}\"",
+                                 assemblyVersion.Major,
+                                 assemblyVersion.Minor,
+                                 assemblyVersion.Build,
+                                 assemblyVersion.Revision);
+        }
+    }
+}
---
+++ RestFoundation/RestFoundation/Runtime/Handlers/JQueryRouteHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Web;
+using System.Web.Routing;
+
+namespace RestFoundation.Runtime.Handlers
+{
+    internal class JQueryRouteHandler : IRouteHandler, IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return this;
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            using (var jqueryStream = typeof(Rest).Assembly.GetManifestResourceStream("RestFoundation.ServiceProxy.Resources.jquery-1.8.3.min.js"))
+            {
+                if (jqueryStream == null)
+                {
+                    throw new HttpException(404, "JQuery resource not found");
+                }
+
+                context.Response.ContentType = "application/x-javascript; charset=utf-8";
+                context.Response.Cache.SetCacheability(HttpCacheability.Public);
+                context.Response.Cache.SetETag(GetAssemblyVersionAsEtag());
+                context.Response.Cache.SetExpires(DateTime.Now.AddYears(1));
+                context.Response.Cache.SetValidUntilExpires(true);
+
+                jqueryStream.CopyTo(context.Response.OutputStream);
+            }
+        }
+
+        private string GetAssemblyVersionAsEtag()
+        {
+            Version assemblyVersion = GetType().Assembly.GetName().Version;
+
+            return String.Format(CultureInfo.InvariantCulture,
+                                 "\"JQ-{0}-{1}-{2}-{3}\"",
+                                 assemblyVersion.Major,
+                                 assemblyVersion.Minor,
+                                 assemblyVersion.Build,
+                                 assemblyVersion.Revision);
+        }
+    }
+}
