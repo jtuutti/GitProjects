c201df9e1876c01af42520450221efa7c22ffab6
Dmitry <Dmitry>
2012-12-27 18:06:55 -0500

    Packaged version 1.2.0.0

--- RestFoundation/build.bat
+++ RestFoundation/build.bat
@@ -10,7 +10,7 @@ cd .\RestFoundation\bin\Release
 echo.
 echo.
 echo Performing IL Merge
-ilmerge /out:Rest.Foundation.dll RestFoundation.dll Newtonsoft.Json.dll /ver:1.1.9.0 /t:library /internalize /targetplatform="v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /keyfile:..\..\..\RestFoundation.snk /xmldocs
+ilmerge /out:Rest.Foundation.dll RestFoundation.dll Newtonsoft.Json.dll /ver:1.2.0.0 /t:library /internalize /targetplatform="v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /keyfile:..\..\..\RestFoundation.snk /xmldocs
 echo.
 echo.
 copy Rest.Foundation.* ..\..\..\build\ /y
