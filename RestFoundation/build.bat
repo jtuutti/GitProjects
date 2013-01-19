f7a63014288c1529df4bd19b6e20b6a138ca166c
Dmitry <Dmitry>
2013-01-19 14:24:29 -0500

    Signed-off-by: Dmitry <Dmitry>

--- RestFoundation/build.bat
+++ RestFoundation/build.bat
@@ -5,14 +5,27 @@ mkdir .\build > nul
 del .\build\Rest.Foundation.* /f /q > nul
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild RestFoundation\RestFoundation.csproj /t:Rebuild /p:Configuration=Release
-cd .\RestFoundation\bin\Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild RestFoundation.Tests\RestFoundation.Tests.csproj /t:Rebuild /p:Configuration=Release
 
 echo.
+echo Running Unit Tests
+"C:\Program Files (x86)\NUnit 2.5.8\bin\net-2.0\nunit-console" D:\Development\GitProjects\RestFoundation\RestFoundation.Tests\bin\Release\RestFoundation.Tests.dll /labels
+
+if %errorlevel% neq 0 goto error
+if %errorlevel% equ 0 goto merge
+
+:error
+echo Error # %errorlevel% !!! Unit Tests Failed !!!
+goto end
+
+:merge
 echo.
 echo Performing IL Merge
+cd .\RestFoundation\bin\Release
 ilmerge /out:Rest.Foundation.dll RestFoundation.dll Newtonsoft.Json.dll /ver:1.2.1.0 /t:library /internalize /targetplatform="v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /keyfile:..\..\..\RestFoundation.snk /xmldocs
-echo.
 echo.
 copy Rest.Foundation.* ..\..\..\build\ /y
 echo.
 pause
+
+:end
