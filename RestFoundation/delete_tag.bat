0d152b36beedae4be730ff53d9329f0d643fc24e
Dmitry <dmitry.starosta@gmail.com>
2013-06-26 00:24:35 -0400

    Packaged version 2.5.0.287

--- RestFoundation/delete_tag.bat
+++ RestFoundation/delete_tag.bat
@@ -1,9 +1,11 @@
 @echo off
 echo Last known tag name:
 git describe --tags --abbrev=0
+if %errorlevel% neq 0 goto noparam
 echo.
 set /p tag="Enter a tag name: "
 if "%tag%"=="" goto noparam
+git tag -d %tag%
 git push https://github.com/dstarosta/GitProjects :%tag%
 git push D:/GitRepo :%tag%
 goto complete
