2c3a37eab91f9ae375d1ef506b2a055fdf923944
Dmitry <dmitry.starosta@gmail.com>
2013-08-05 21:24:39 -0400

    Packaged version 2.5.0.326

--- RestFoundation/RestFoundation/Properties/VersionInfo.cs
+++ RestFoundation/RestFoundation/Properties/VersionInfo.cs
@@ -13,6 +13,6 @@ namespace RestFoundation
         /// <summary>
         /// Gets the build version.
         /// </summary>
-        public const string BuildVersion = "2.5.0.323";
+        public const string BuildVersion = "2.5.0.326";
     }
 }
