6379d349affa8666ea8be03884ca7629a54a52c9
Dmitry <dmitry@starosta.net>
2014-08-13 01:08:26 -0400

    Improved catch all route

--- SimpleViewEngine/SimpleViewEngine.Example/App_Start/RouteConfig.cs
+++ SimpleViewEngine/SimpleViewEngine.Example/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SimpleViewEngine.Example
@@ -15,12 +11,11 @@ namespace SimpleViewEngine.Example
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}/{*catchAll}",
                 defaults: new
                 {
                     controller = "Home",
-                    action = "Index",
-                    id = UrlParameter.Optional
+                    action = "Index"
                 }
             );
         }
