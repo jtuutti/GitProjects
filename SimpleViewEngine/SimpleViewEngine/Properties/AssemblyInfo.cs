2733d1af7e48ab9dd086c8709b1baa991c85b04c
Dmitry <dmitry@starosta.net>
2014-12-19 00:01:59 -0500

    Incremented version

--- SimpleViewEngine/SimpleViewEngine/Properties/AssemblyInfo.cs
+++ SimpleViewEngine/SimpleViewEngine/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
-[assembly: AssemblyInformationalVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyInformationalVersion("1.0.4.0")]
